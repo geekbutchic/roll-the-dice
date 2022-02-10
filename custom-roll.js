af1f6de59a36b074c59b57775ceeb82173bac74a
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-02-10 09:13:16 -0700

    adds custom-roll.js and solution

--- custom-roll.js
+++ custom-roll.js
@@ -1,7 +1,6 @@
 const getRandomNumber = require("./six-sided");
 const prompt = require("prompt-sync")();
 
-
 let choice = Number(prompt("Choose how many sides the dice should have: "));
 
 console.log(getRandomNumber(choice));
---
+++ rigged-dice.js
@@ -0,0 +1,6 @@
+const getRandomNumber = require("./six-sided");
+const prompt = require("prompt-sync")();
+
+let sides = Number((prompt("Choose the number of sides")));
+
+let num = Number((prompt("Choose ")))
